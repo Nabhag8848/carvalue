466b0f1cac0b03201bc7c1a002cdf62c47415895
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-09-21 16:52:36 +0530

    apply validation and cookie on app-module to support in e2e

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['sfosdhgsdoghdsoufhoifhdisuhfdsiuh'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   await app.listen(3000);
 }
 bootstrap();
