7f77fa2590eb34c9858cf672ad41586f74249495
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-09-23 16:44:07 +0530

    have cookie key as env to support different in prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -49,11 +49,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['sfosdhgsdoghdsoufhoifhdisuhfdsiuh'],
+          keys: [this.configService.get<string>('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
